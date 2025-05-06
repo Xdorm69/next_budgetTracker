e4b4481c91a5bbcdf6f208e7f3df38dfcc68b0f9
Amitoj <amitoj.mehta@gmail.com>
2025-05-06 11:53:24 +0530

    first commit

--- components/ui/date-range-picker.tsx
+++ components/ui/date-range-picker.tsx
@@ -299,7 +299,7 @@ export const DateRangePicker: FC<DateRangePickerProps> & {
     preset: string;
     label: string;
     isSelected: boolean;
-  }): JSX.Element => (
+  }) => (
     <Button
       className={cn(isSelected && "pointer-events-none")}
       variant="ghost"
